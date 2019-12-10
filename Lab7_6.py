046ebb109d475488b2d4d610ddfe1fe37441e89f
Shapoval <shapoval.sergey@mail.ru>
2019-12-10 10:00:58 +0700

    Закончили работу над 6 файлом

---
+++ Lab7_6.py
@@ -0,0 +1,17 @@
+import time
+import math
+from selenium import webdriver
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+link = "http://suninjuly.github.io/alert_accept.html"
+driver = webdriver.Chrome()
+driver.get(link)
+option3 = driver.find_element_by_tag_name('button')
+option3.submit()
+x_element = driver.find_element_by_id('input_value')
+x = x_element.text
+y = calc(x)
+input1 = driver.find_element_by_id('answer')
+input1.send_keys(y)
+option3 = driver.find_element_by_tag_name('button')
+option3.submit()
